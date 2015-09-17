68ac300490755b4faec2b6c43f7e61aba1eaddc9
Brian Haggard <brihagg@gmail.com>
2015-09-17 09:59:47 -0500

    Start plumbing for asynchronious environment calls

--- src/conserl_env_poller.erl
+++ src/conserl_env_poller.erl
@@ -2,7 +2,10 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([
+         start_link/0,
+         set_env/1
+        ]).
 
 -export([init/1,
          handle_call/3,
@@ -12,6 +15,10 @@
          code_change/3]).
 
 -define(TIMEOUT, 30000).
+
+-spec set_env(Env::map()) -> ok.
+set_env(Env) ->
+  gen_server:cast(?MODULE, {set_env, Env}).
 
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -25,14 +32,16 @@ init([]) ->
 handle_call(_Request, _From, State) ->
   {reply, ok, State, ?TIMEOUT}.
 
+handle_cast({set_env, Env}, State) ->
+  conserl_env_setter:set_env(Env),
+  Index = conserl_env_http:index(Env),
+  {noreply, State#{index => Index}, ?TIMEOUT};
 handle_cast(_Msg, State) ->
   {noreply, State, ?TIMEOUT}.
 
 handle_info(timeout, State = #{index := Index}) ->
-  Env = conserl_env_http:get_env(Index),
-  conserl_env_setter:set_env(Env),
-  Index1 = conserl_env_http:index(Env),
-  {noreply, State#{index => Index1}, ?TIMEOUT};
+  ok = conserl_env_http:get_env(Index),
+  {noreply, State, ?TIMEOUT};
 handle_info(_Info, State) ->
   {noreply, State, ?TIMEOUT}.
 
--- test/conserl_env_poller_tests.erl
+++ test/conserl_env_poller_tests.erl
@@ -2,10 +2,12 @@
 
 -include("test_fixture.hrl").
 
+-define(DEFAULT_TIMEOUT, 30000).
+
 setup() ->
   ?meck([conserl_env_http, conserl_env_setter], [non_strict]),
   ?stub(conserl_env_http, get_env, 0, environment1),
-  ?stub(conserl_env_http, get_env, 1, environment1),
+  ?stub(conserl_env_http, get_env, 1, ok),
   ?stub(conserl_env_http, index, 1, index1),
   ?stub(conserl_env_setter, set_env, 1, ok).
 
@@ -29,36 +31,44 @@ handle_info_when_timeout_calls_consul_api() ->
 
   ?called(conserl_env_http, get_env, [index2]).
 
-handle_info_when_timeout_stores_last_index_in_state() ->
+handle_info_when_timeout_preserves_state() ->
+  State = #{index => index2, stuff => things, key1 => value1},
+  ?assertMatch({noreply, State, _}, conserl_env_poller:handle_info(timeout, State)).
+
+handle_cast_when_set_env_stores_last_index_in_state() ->
   ?stub(conserl_env_http, index, 1, index3),
-  ?assertMatch({noreply, #{index := index3}, _}, conserl_env_poller:handle_info(timeout, #{index => index2})),
+  ?assertMatch({noreply, #{index := index3}, _}, conserl_env_poller:handle_cast({set_env, environment1}, #{index => index2})),
 
   ?called(conserl_env_http, index, [environment1]).
 
-handle_info_when_timeout_preserves_state() ->
+handle_cast_when_set_env_preserves_state() ->
   ?stub(conserl_env_http, index, 1, index3),
   State = #{index => index2, stuff => things, key1 => value1},
   ExpectedState = State#{index := index3},
-  ?assertMatch({noreply, ExpectedState, _}, conserl_env_poller:handle_info(timeout, State)),
+  ?assertMatch({noreply, ExpectedState, _}, conserl_env_poller:handle_cast({set_env, environment1}, State)),
 
   ?called(conserl_env_http, index, [environment1]).
 
-handle_info_when_timeout_calls_env_setter_with_environment_informtion() ->
-  conserl_env_poller:handle_info(timeout, #{index => index2}),
+handle_cast_when_set_env_calls_env_setter_with_environment_informtion() ->
+  conserl_env_poller:handle_cast({set_env, environment1}, #{index => index2}),
 
   ?called(conserl_env_setter, set_env, [environment1]).
 
 init_when_timeout_returns_timeout() ->
-  ?assertMatch({ok, _, 30000}, conserl_env_poller:init([])).
+  ?assertMatch({ok, _, ?DEFAULT_TIMEOUT}, conserl_env_poller:init([])).
 
 handle_call_returns_timeout() ->
-  ?assertMatch({reply, _, state1, 30000}, conserl_env_poller:handle_call(any_req, from_me, state1)).
+  ?assertMatch({reply, _, state1, ?DEFAULT_TIMEOUT}, conserl_env_poller:handle_call(any_req, from_me, state1)).
 
 handle_cast_returns_timeout() ->
-  ?assertMatch({noreply, state1, 30000}, conserl_env_poller:handle_cast(any_msg, state1)).
+  ?assertMatch({noreply, state1, ?DEFAULT_TIMEOUT}, conserl_env_poller:handle_cast(any_msg, state1)).
 
 handle_info_returns_timeout() ->
-  ?assertMatch({noreply, state1, 30000}, conserl_env_poller:handle_info(any_info, state1)).
+  ?assertMatch({noreply, state1, ?DEFAULT_TIMEOUT}, conserl_env_poller:handle_info(any_info, state1)).
 
 handle_info_when_timeout_returns_timeout() ->
-  ?assertMatch({noreply, _, 30000}, conserl_env_poller:handle_info(timeout, #{index => index1})).
+  ?assertMatch({noreply, _, ?DEFAULT_TIMEOUT}, conserl_env_poller:handle_info(timeout, #{index => index1})).
+
+handle_cast_when_set_env_returns_timeout() ->
+  ?assertMatch({noreply, _, ?DEFAULT_TIMEOUT}, conserl_env_poller:handle_cast({set_env, ignored}, #{index => index1})).
+
