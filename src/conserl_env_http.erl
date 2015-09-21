4b17a55df7afd268442574bcda9aa72ab04009d5
Mike Beyer <miketbeyer@gmail.com>
2015-09-21 13:46:56 -0500

    refactor join get_env functions logic together

--- src/conserl_env_http.erl
+++ src/conserl_env_http.erl
@@ -10,28 +10,25 @@
 
 -spec get_env() -> {Index::non_neg_integer, [{Application::atom(), Key::atom(), Value::term()}]}.
 get_env() ->
-  Uri = build_uri(),
-  {ok, _, [{"X-Consul-Index", Index}], Body} = ibrowse:send_req(Uri, ?HEADERS, get),
-  ListOfKVs = jiffy:decode(Body, [return_maps]),
-  ListOfAppEnvs = [ conserl_env_http_parser:parse_kv(X) || X <- ListOfKVs ],
-  {list_to_integer(Index), ListOfAppEnvs}.
+  get_env1(0).
 
 -spec get_env(Index::non_neg_integer()) -> Environment::map().
 get_env(Index) ->
-  Uri = build_uri(Index),
-  spawn_link(fun() ->
-               {ok, _, [{"X-Consul-Index", Index1}], Body} = ibrowse:send_req(Uri, ?HEADERS, get),
-               ListOfKVs = jiffy:decode(Body, [return_maps]),
-               ListOfAppEnvs = [ conserl_env_http_parser:parse_kv(X) || X <- ListOfKVs ],
-               conserl_env_poller:set_env({list_to_integer(Index1), ListOfAppEnvs})
+  spawn_link(fun() -> 
+                 Env = get_env1(Index),
+                 conserl_env_poller:set_env(Env)
              end).
+
+get_env1(Index) ->
+  Uri = build_uri(Index),
+  {ok, _, [{"X-Consul-Index", Index1}], Body} = ibrowse:send_req(Uri, ?HEADERS, get),
+  ListOfKVs = jiffy:decode(Body, [return_maps]),
+  ListOfAppEnvs = [ conserl_env_http_parser:parse_kv(X) || X <- ListOfKVs ],
+  {list_to_integer(Index1), ListOfAppEnvs}.
 
 -spec index({Index::non_neg_integer(), [{Application::atom(), Key::atom(), Value::term()}]}) -> Index::non_neg_integer().
 index({Index, []}) ->
   Index.
-
-build_uri() ->
-  build_uri(0).
 
 build_uri(Index) ->
   {ok, Tld}  = application:get_env(conserl_env, consul_tld,  "local"),
